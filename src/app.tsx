843a708bcc09dbd149467d92bae5ebecf4918017
RafaelZucarelli <zucarelli83@gmail.com>
2024-02-22 17:31:47 -0300

    feat: pagination

--- src/app.tsx
+++ src/app.tsx
@@ -6,8 +6,39 @@ import { Button } from './components/ui/button'
 import { Control, Input } from './components/ui/input'
 import { Table, TableHeader, TableRow, TableHead, TableCell, TableBody } from './components/ui/table'
 import { Pagination } from './components/pagination'
+import { useQuery } from '@tanstack/react-query'
+
+export interface TagResponse {
+  first: number
+  prev: number | null
+  next: number
+  last: number
+  pages: number
+  items: number
+  data: Tag[]
+}
+
+export interface Tag {
+  title: string
+  slug: string
+  amountOfVideos: number
+  id: string
+}
 
 export function App() {
+  const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+    queryKey: ['get-tags'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3333/tags?_page=1&_per_page=10')
+      const data = await response.json()
+
+      return data
+    },
+  })
+
+  if (isLoading) {
+    return null
+  }
   return (
     <div className="py-10 space-y-8">
     <div>
@@ -51,18 +82,18 @@ export function App() {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {Array.from({ length: 10 }).map((value, index) => {
+          {tagsResponse ?.data.map((tag) => {
             return (
-              <TableRow key={index}>
+              <TableRow key={tag.id}>
             <TableCell></TableCell>
             <TableCell>
               <div className="flex flex-col gap-0.5">
-                <span className="font-medium">React</span>
-                <span className="text-xs text-zinc-500">9E0E95BF-4BF6-4A23-B79E-F71AE9C9C117</span>
+                <span className="font-medium">{tag.title}</span>
+                <span className="text-xs text-zinc-500">{tag.id}</span>
               </div>
             </TableCell>
             <TableCell className="text-zinc-300">
-              13 video(s)
+              {tag.amountOfVideos} video(s)
             </TableCell>
             <TableCell className="text-right">
               <Button size="icon">
@@ -75,11 +106,8 @@ export function App() {
         </TableBody>
       </Table>
 
-      <Pagination />
+          {tagsResponse && <Pagination pages={tagsResponse.pages} items={tagsResponse.items} page={1} />}
 
-      
-
-      
       </main>
   </div>
   ) 
